0c7095851f2783a825b0f1229b785b8c22f84a31
nimazein <nimazein@edu.hse.ru>
2019-06-03 02:06:55 +0500

    Task 4 solved, task 2 failed

---
+++ Educational practice/Task 2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task_2
+{
+    class Program
+    {
+        static int n;
+        static StreamWriter writer;
+        static List<List<string>> answer;
+        static void Main(string[] args)
+        {
+            GetN();
+            initializeList();
+            GetStreamWriter();
+            Solve();
+            CloseStreams();
+
+        }
+        private static void GetN()
+        {
+            FileStream input = new FileStream("input.txt", FileMode.Open);
+            StreamReader reader = new StreamReader(input);
+
+            string inputStr = reader.ReadToEnd();
+            n = Convert.ToInt32(inputStr);
+            reader.Close();
+            input.Close();
+        }
+        private static void GetStreamWriter()
+        {
+            FileStream output = new FileStream("output.txt", FileMode.OpenOrCreate);
+            writer = new StreamWriter(output);
+        }
+        private static void initializeList()
+        {
+            answer = new List<List<string>>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                answer.Add(new List<string>());
+            }
+        }
+        private static void Solve()
+        {
+            if (n > 170)
+            {
+                writer.WriteLine("Невозможно");
+                return;
+            }
+            else if (n == 170)
+            {
+                writer.Write("T20 T20 Bull");
+                return;
+            }
+            else
+            {
+                while (n > 61)
+                {
+                    n -= 60;
+                    answer[0].Add("T20");
+                }
+
+                if (n <= 40)
+                {
+                    if (n % 2 == 0)
+                    {
+                        ("D" + n / 2 + "\n");
+                    }
+                    else
+                    {
+                        output.Append(1 + "\n");
+                        n -= 1;
+                        output.Append("D" + n / 2 + "\n");
+                    }
+                }
+                else
+                {
+                    output.Append(n - 40 + "\n");
+                    n = 40;
+                    output.Append("D" + n / 2 + "\n");
+                }
+
+            }
+
+            for (int i = 0; i < answer.Count; i++)
+            {
+                for (int j = 0; j < answer[i].Count; j++)
+                {
+                    writer.Write(answer[i][j] + " ");
+                }
+                writer.WriteLine("");
+            }
+        }
+        private static void CloseStreams()
+        {
+            writer.Close();
+        }
+    }
+}
---
+++ Educational practice/Task 4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger value = CountValue();
+            OutputValue(value);
+            OutputSeries(value);
+            Console.ReadKey();
+        }
+        private static BigInteger CountValue()
+        {
+            BigInteger bigInteger = new BigInteger();
+            bigInteger = 2;
+            for (int i = 0; i < 200; i++)
+            {
+                bigInteger *= 2;
+            }
+
+            return bigInteger;
+        }
+        private static void OutputValue(BigInteger value)
+        {
+            string s = value.ToString();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        private static void OutputSeries(BigInteger value)
+        {
+            string s = value.ToString();
+
+            Console.Write(s[0] + $"*10^{s.Length - 1} ");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write("+ " + s[i] + $"*10^{s.Length - (i + 1)} ");
+            }
+            Console.WriteLine("= 2^200");
+        }
+    }
+}
