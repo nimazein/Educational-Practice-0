3dc798e0c6cdd9ecde8e29d5b9249154a7be63cf
nimazein <nimazein@edu.hse.ru>
2019-06-22 10:36:42 +0500

    Task 11 solved

---
+++ Educational practice/Task 11/Dictionary.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Dictionary
+    {
+        private List<string> symbols = new List<string>();
+        private List<string> codes = new List<string>();
+
+        public List<string> Symbols
+        {
+            get
+            {
+                return symbols;
+            }
+                
+        }
+        public List<string> Codes
+        {
+            get
+            {
+                return codes;
+            }
+
+
+        }
+        public Dictionary()
+        {
+
+        }
+        
+        public void FillDictionary()
+        {
+            // A-Z
+            for (int i = 65; i <= 90; i++)
+            {
+                string code = ToBinary(i - 65);
+                codes.Add(code);
+                symbols.Add(Convert.ToString(Convert.ToChar(i)));
+            }
+
+            //a-z
+            for (int i = 97; i <= 122; i++)
+            {
+                string code = ToBinary(i - 97 + 26);
+                codes.Add(code);
+                symbols.Add(Convert.ToString(Convert.ToChar(i)));
+            }
+
+            //numbers
+            for (int i = 0; i <= 9; i++)
+            {
+                string code = ToBinary(i + 52);
+                codes.Add(code);
+                symbols.Add(i.ToString());
+            }
+
+           
+
+            codes.Add(ToBinary(62));
+            symbols.Add(".");
+
+            codes.Add(ToBinary(63));
+            symbols.Add(",");
+
+            codes.Add(ToBinary(64));
+            symbols.Add("!");
+
+            codes.Add(ToBinary(65));
+            symbols.Add(":");
+
+            codes.Add(ToBinary(66));
+            symbols.Add(";");
+
+            codes.Add(ToBinary(67));
+            symbols.Add("?");
+
+            codes.Add(ToBinary(68));
+            symbols.Add("\n");
+
+            codes.Add(ToBinary(69));
+            symbols.Add(" ");
+
+        }
+        public string ToBinary(int num)
+        {
+            string stumpOfCode = Convert.ToString(num, 2);
+
+            StringBuilder fullCode = new StringBuilder(stumpOfCode);
+
+            while (fullCode.Length != 7)
+            {
+                fullCode.Insert(0, "0");
+            }
+
+            return fullCode.ToString();
+
+        }
+
+
+    }
+}
---
+++ Educational practice/Task 11/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Program
+    {
+        /* О шифре
+         
+             Кодируются английские заглавные и строчные буквы, пробел и знаки препинания.
+             Каждый из символов кодируется двоичным числом длиной в 7 символов.
+             Кодирование происходит всеми числами двоичной системы, начиная с 000000 
+             и заканчивая 111111, кодируемые символы расположены в порядке:
+             1) Заглавные буквы
+             2) Строчные буквы
+             3) Цифры
+             4) Пробел
+             
+             Пример: Символ 'А' кодируется числом 000000, 'D' - 000011
+         */
+
+        static string text;
+        static Dictionary dictionary = new Dictionary();
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            dictionary.FillDictionary();
+            ReadText();
+            Console.WriteLine("Закодированный текст: ");
+            string codedText = CodeText();
+            Console.ReadKey();
+
+            Console.WriteLine("Раскодированный текст: ");
+            DecodeString(codedText);
+            Console.ReadKey();
+                
+        }
+        static void ReadText()
+        {
+            text = @"Twinkle, twinkle, little star,
+How I wonder what you are.
+Up above the world so high,
+Like a diamond in the sky.";
+        }
+        static string CodeText()
+        {
+            StringBuilder codedText = new StringBuilder("");
+
+            foreach (char symbol in text)
+            {
+                if (dictionary.Symbols.Contains(symbol.ToString()))
+                {
+                    int idx = dictionary.Symbols.IndexOf(symbol.ToString());
+                    codedText.Append(dictionary.Codes[idx] + " ");
+                }
+                else
+                {
+                    int idx = dictionary.Symbols.IndexOf("\n");
+                    codedText.Append(dictionary.Codes[idx] + " ");
+                }
+            }
+            Console.WriteLine(codedText);
+            return codedText.ToString();
+        }
+        static void DecodeString(string codedText)
+        {
+            string[] codeWords = codedText.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder uncoded = new StringBuilder("");
+
+            foreach (string codeWord in codeWords)
+            {
+                try
+                {
+                    int idx = dictionary.Codes.IndexOf(codeWord);
+                    uncoded.Append(dictionary.Symbols[idx]);
+                }
+                catch
+                {
+                    Console.WriteLine("GG");
+                }
+                
+            }
+
+            Console.WriteLine(uncoded);
+        }
+    }
+}
