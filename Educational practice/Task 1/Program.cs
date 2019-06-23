5973d22f76436fb9af1763991f882697659a7a0d
nimazein <nimazein@edu.hse.ru>
2019-06-24 02:17:46 +0500

    Task 1 solved

--- Educational practice/Task 1/Program.cs
+++ Educational practice/Task 1/Program.cs
@@ -1,131 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Task_1
+namespace Task_1_v2
 {
     class Program
     {
         static StreamReader reader = new StreamReader("INPUT.TXT");
-        static int[] masses;
-        static char[] signs;
-        static double[] percentages;
-        static int[] newPercentages;
-
         static void Main(string[] args)
         {
-            FillMassesAndSigns();
-            int productMass = CountProductMass();
-            FillPercentages(productMass);
-            RoundPercentages();
-            OutputAnswer();
-        }
-        private static void FillMassesAndSigns()
-        {
             int numberOfIngredients = Convert.ToInt32(reader.ReadLine());
+            double[] percentages = new double[numberOfIngredients];
 
-            masses = new int[numberOfIngredients];
-            signs = new char[numberOfIngredients];
-
-            for (int i = 0; i < numberOfIngredients; i++)
-            {
-                InsertElement(i);
-            }
-        }
-        private static void InsertElement(int i)
-        {
-            string currentLine = reader.ReadLine();
-
-            char sign = currentLine[0];
-            signs[i] = sign;
-
-            int substringLength = currentLine.Length - 2;
-            int mass = Convert.ToInt32(currentLine.Substring(2, substringLength));
-            masses[i] = mass;
-        }
-        private static int CountProductMass()
-        {
-            int productMass = 0;
-            foreach (int ingredientMass in masses)
-            {
-                productMass += ingredientMass;
-            }
-            return productMass;
-        }
-        private static void FillPercentages(int productMass)
-        {
-            int numberOfIngredients = masses.Length;
-            percentages = new double[numberOfIngredients];
+            double sumMass = 0;
+            int savedPercent = 0;
+            bool[] good = new bool[numberOfIngredients];
+            bool[] bad = new bool[numberOfIngredients];
 
             for (int i = 0; i < numberOfIngredients; i++)
             {
-                double ingredientMass = Convert.ToDouble(masses[i]);
-                double percentage = (ingredientMass / Convert.ToDouble(productMass)) * 100;
+                string currentLine = reader.ReadLine();
+                good[i] = (currentLine[0] == '+');
 
-                percentages[i] = percentage;
+                int substringLength = currentLine.Length - 2;
+                percentages[i] = Convert.ToInt32(currentLine.Substring(2, substringLength));
+                sumMass += percentages[i];
             }
-        }
-        private static void RoundPercentages()
-        {
-            newPercentages = new int[masses.Length];
-            // Using Math rules
-            for (int i =0; i < masses.Length; i++)
+
+            for (int i = 0; i < numberOfIngredients; i++)
             {
-                if (signs[i] == '+')
-                {
-                    newPercentages[i] = Convert.ToInt32(Math.Ceiling(percentages[i]));
-                }
-                else
-                {   
-                    newPercentages[i] = Convert.ToInt32(Math.Floor(percentages[i]));
-                }
+                percentages[i] = 100 * percentages[i] / sumMass;
+                bad[i] = !(percentages[i] > Math.Floor(percentages[i]));
+                savedPercent += Convert.ToInt32(Math.Floor(percentages[i]));
             }
 
-            // To meet conditions
-            int sum = CountSum(); 
-            int k = 0;
-            while (sum != 100)
+            int[] additionalValue = new int[numberOfIngredients];
+            savedPercent = 100 - savedPercent;
+
+            for (int j = 0; j < 2; j++)
             {
-                if (k == percentages.Length)
-                    k = 0;
-
-                if (sum > 100)
+                for (int i = 0; i < numberOfIngredients; i++)
                 {
-                    if (signs[k] == '+')
-                    {
-                        newPercentages[k]--;
-                    }
-                }
-                else
-                {
-                    if (signs[k] == '-')
-                    {
-                        newPercentages[k]++;
-                    }
-                }
+                    if (bad[i] || good[i] ^ (j == 0))
+                        continue;
+                    if (--savedPercent < 0)
+                        break;
+                    additionalValue[i] = 1;
 
-                sum = CountSum();
-                k++;
+                }
             }
+
+            sumMass = 0;
+            for (int i = 0; i < numberOfIngredients; i++)
+            {
+                percentages[i] = (Math.Floor(percentages[i]) + additionalValue[i]);
+                sumMass += percentages[i];
+            }
+
+            if (sumMass != 100)
+            {
+                Console.WriteLine("Error");
+                Console.ReadKey();
+            }
+
+            OutputAnswer(percentages);
         }
-        private static void OutputAnswer()
+        private static void OutputAnswer(double[] percentages)
         {
             using (System.IO.StreamWriter file =
                 new System.IO.StreamWriter("OUTPUT.txt"))
             {
-                foreach (int percentage in newPercentages)
+                foreach (double percentage in percentages)
                 {
                     file.WriteLine(percentage.ToString());
                 }
             }
-        }
-        private static int CountSum()
-        {
-            int sum = 0;
-            foreach (int el in newPercentages)
-            {
-                sum += el;
-            }
-            return sum;
         }
     }
 }
